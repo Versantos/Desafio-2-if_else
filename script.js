d04119061ffbb8e7df9f59af0c96eae2cdd88b17
Versantos <veronicasilvamagalhaes@gmail.com>
2022-01-26 19:04:47 -0300

    Finalizado

---
+++ script.js
@@ -0,0 +1,42 @@
+let faixaEtaria = 18;
+
+if( faixaEtaria == "18"){
+    console.log('18');
+}else{
+    console.log('maior de idade')
+}
+
+let maioridade = "30";
+
+if( maioridade == "30"){
+    console.log('30');
+}else{
+    console.log('Menor de idade!')
+}
+
+let aniversario = "janeiro"
+
+if( aniversario == "janeiro"){
+    console.log("Parabéns, janeiro é seu niver")
+    }else if(aniversario != "dezembro"){
+        console.log("Errei, data do seu aniversário")
+    }else{
+        console.log("Quando é seu aniversário mesmo!?")
+    }
+
+    let persona = "Veronica"
+
+    if( persona.substring (0,1 === "R")){
+        console.log("Seu nome começa com a letra")
+    }else{
+        console.log("Seu nome não começa com a letra")
+    }
+    
+    
+    let lastName = "Santos"
+
+    if( lastName.length < 6 ){
+        console.log("SobreNome tem seis letras")
+    }else{
+        console.log("Nome com seis letras")
+    }
